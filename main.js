9fd42ee97b17c2dc079d80928c7c8afe93f724f1
bourich1 <bourichanouar@gmail.com>
2023-11-21 12:16:07 +0100

    site web store iphone prudacts

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var photo = document.querySelector(".phone");
+
+var colors = document.querySelector(".container");
+
+function modifierPhoto(src){
+    photo.src = src;
+}
+
+function modifierColor(color){
+    colors.style.background= color;
+}
